ad84f74bfbc820d7780e8b4b410f95c408b62bc9
Tony Delarue <tony.delarue@inria.fr>
2021-11-03 12:39:27 +0100

    ijv2csc without sorted loc2glob

--- src/z_spm_convert_to_csc.c
+++ src/z_spm_convert_to_csc.c
@@ -19,6 +19,290 @@
 #include "common.h"
 #include "z_spm.h"
 
+#if defined(SPM_WITH_MPI)
+/**
+ * @brief Convert a matrix in IJV format to a matrix in CSC format for a
+ *        distributed variadic multidof spm without a sorted distribution.
+ *
+ * @param[inout] spm
+ *          The matrix to convert in CSC format.
+ *
+ * @param[in] oldrow
+ *          Pointer to the old rowptr in IJV format.
+ *
+ * @param[in] oldval
+ *          Pointer to the old sorted values ptr.
+ *
+ * @param[in] l2g_sorted
+ *          Represents the corresponding sorted distribution of the spm.
+ */
+static inline void
+z_spm_dijv2csc_vdof( spmatrix_t            *spm,
+                     const spm_int_t       *oldrowptr,
+                     const spm_complex64_t *oldval,
+                     const spm_int_t       *l2g_sorted )
+{
+    const spm_int_t *glob2loc;
+    const spm_int_t *newcol;
+    const spm_int_t *oldrow;
+    spm_int_t       *newrow;
+    spm_int_t        k, j, jl, jg, baseval;
+    size_t           size;
+#if !defined(PRECISION_p)
+    spm_complex64_t *newval;
+    spm_int_t       *dofshift = NULL;
+#else
+    (void)oldval;
+#endif
+    baseval  = spm->baseval;
+    glob2loc = spm->glob2loc;
+    newcol   = spm->colptr;
+    newrow   = spm->rowptr;
+    oldrow   = oldrowptr;
+    size     = 0;
+
+    /*
+     * Copy the datas from the sorted rowptr to
+     * its unsorted distribution counterpart
+     */
+    for ( j = 0; j < spm->n; j++, oldrow+=size )
+    {
+        jg   = l2g_sorted[j] - baseval;
+        jl   = glob2loc[jg];
+        k    = newcol[jl] - baseval;
+        size = newcol[jl+1] - newcol[jl];
+        memcpy( newrow + k, oldrow, size * sizeof(spm_int_t) );
+    }
+    assert( (oldrow - oldrowptr) == spm->nnz );
+
+#if !defined(PRECISION_p)
+    /*
+     * Copy the datas from the sorted values to
+     * its unsorted distribution counterpart
+     */
+    oldrow = oldrowptr;
+    newval = spm->values;
+
+    /* Get the values shift for the new spm */
+    dofshift = spm_get_value_idx_by_col( spm );
+    for ( j = 0; j < spm->n; j++ )
+    {
+        jg   = l2g_sorted[j] - baseval;
+        jl   = glob2loc[jg];
+        size = dofshift[jl+1] - dofshift[jl];
+
+#if !defined(NDEBUG)
+        /* Check that we compute the same size as in spm_value_idx_by_col() */
+        {
+            const spm_int_t *dofs = spm->dofs;
+            spm_int_t        ig, dofi, dofj;
+
+            dofj = dofs[jg+1] - dofs[jg];
+
+            dofi = 0;
+            for( k = newcol[jl]; k < newcol[jl+1]; k++, oldrow++ )
+            {
+                ig    = *oldrow - baseval;
+                dofi += dofs[ig+1] - dofs[ig];
+            }
+
+            /* Get the position and size of the column */
+            assert( (size_t)(dofi * dofj) == size );
+        }
+#endif
+
+        memcpy( newval + dofshift[jl], oldval, size * sizeof(spm_complex64_t) );
+        oldval += size;
+    }
+    assert( (oldrow - oldrowptr) == spm->nnz );
+
+    free( dofshift );
+#endif
+}
+
+/**
+ * @brief convert a matrix in IJV format to a matrix in CSC
+ * format for a distributed constant multidof spm without a sorted distribution.
+ *
+ * @param[inout] spm
+ *          The matrix to convert in CSC format.
+ *
+ * @param[inout] oldcol
+ *          Pointer to the old IJV colptr. Will store the new rowptr.
+ *
+ * @param[in] l2g_sorted
+ *          Represents the corresponding sorted distribution of the spm.
+ */
+static inline void
+z_spm_dijv2csc_cdof( spmatrix_t            *spm,
+                     const spm_int_t       *oldrowptr,
+                     const spm_complex64_t *oldval,
+                     const spm_int_t       *l2g_sorted )
+{
+    const spm_int_t *glob2loc;
+    const spm_int_t *newcol;
+    const spm_int_t *oldrow;
+    spm_int_t       *newrow;
+    spm_int_t        k, j, jl, jg, baseval;
+    size_t           size;
+#if !defined(PRECISION_p)
+    spm_complex64_t *newval;
+    spm_int_t        dof2;
+
+    newval = spm->values;
+    dof2   = spm->dof * spm->dof;
+#else
+    (void)oldval;
+#endif
+    baseval  = spm->baseval;
+    glob2loc = spm->glob2loc;
+    newcol   = spm->colptr;
+    newrow   = spm->rowptr;
+    oldrow   = oldrowptr;
+    size     = 0;
+
+    /*
+     * Copy the datas from the sorted rowptr (and values) to
+     * its unsorted distribution counterpart
+     */
+    for ( j = 0; j < spm->n; j++, oldrow+=size )
+    {
+        jg   = l2g_sorted[j] - baseval;
+        jl   = glob2loc[jg];
+        k    = newcol[jl] - baseval;
+        size = newcol[jl+1] - newcol[jl];
+        memcpy( newrow + k, oldrow, size * sizeof(spm_int_t) );
+
+#if !defined(PRECISION_p)
+        memcpy( newval + k * dof2, oldval,
+                size * dof2 * sizeof(spm_complex64_t) );
+        oldval += size * dof2;
+#endif
+    }
+    assert( (oldrow - oldrowptr) == spm->nnz );
+}
+
+/**
+ * @brief convert a matrix in IJV format to a matrix in CSC
+ *        format for a distributed spm.
+ *
+ * @param[inout] spm
+ *          The matrix to convert in CSC format.
+ *
+ * @retval SPM_SUCCESS on success
+ * @retval SPM_ERR_NOTIMPLEMENTED on non supported cases
+ */
+static inline int
+z_spm_dijv2csc( spmatrix_t *spm )
+{
+    spm_int_t *newcol, *oldcol;
+    spm_int_t *glob2loc, *l2g_sorted;
+    spm_int_t  k, j, jg, baseval;
+    int        is_sorted;
+    int        distribution = spm_get_distribution( spm );
+
+    if ( !(distribution & SpmDistByColumn) ) {
+        fprintf( stderr, "spmConvert: Conversion of non column distributed matrices to CSC is not yet implemented\n");
+        return SPM_ERR_BADPARAMETER;
+    }
+    baseval = spm->baseval;
+
+    /* Allocate and compute the glob2loc array */
+    glob2loc = spm_get_glob2loc( spm );
+
+    /* Allocate and compute the new colptr */
+    newcol = (spm_int_t *) calloc(spm->n+1,sizeof(spm_int_t));
+
+    /* Store a sorted version of the loc2glob */
+    l2g_sorted = (spm_int_t *) malloc(spm->n * sizeof(spm_int_t));
+    memcpy( l2g_sorted, spm->loc2glob, spm->n * sizeof(spm_int_t) );
+    spmIntSort1Asc1( l2g_sorted, spm->n );
+
+    /* Compute the number of edges per row */
+    oldcol = spm->colptr;
+    {
+        for ( k=0; k<spm->nnz; k++, oldcol++ )
+        {
+            jg = *oldcol - baseval;
+            j = glob2loc[ jg ];
+            assert( j >= 0 );
+            newcol[j]++;
+        }
+    }
+
+    /* Update the colptr */
+    oldcol      = spm->colptr;
+    spm->colptr = newcol;
+    {
+        spm_int_t  total, tmp;
+        total     = baseval;
+        for (j=0; j<(spm->n+1); j++, newcol++)
+        {
+            tmp = *newcol;
+            *newcol = total;
+            total += tmp;
+        }
+       assert( (total-baseval) == spm->nnz );
+    }
+
+    /* Check if the loc2glob is sorted to avoid unecessary computations */
+    {
+        spm_int_t *loc2glob = spm->loc2glob;
+        is_sorted = 1;
+        for ( j=0; j<(spm->n-1); j++, loc2glob++ )
+        {
+            if ( loc2glob[0] > loc2glob[1] ) {
+                is_sorted = 0;
+                break;
+            }
+        }
+    }
+
+    if ( is_sorted ) {
+        /*
+         * There is nothing more to do.
+         * The old colptr can be freed.
+         */
+        free( oldcol );
+    }
+    else {
+        /*
+         * Update the rowptr/values
+         * The old colptr will be will be used to store the new rowptr.
+         * The old rowptr and old values will be freed.
+         */
+        spm_complex64_t *oldval = spm->values;
+        spm_int_t       *oldrow = spm->rowptr;
+
+        spm->rowptr = oldcol;
+#if !defined(PRECISION_p)
+        spm->values = malloc( spm->nnzexp * sizeof(spm_complex64_t) );
+#endif
+
+        /*
+         * The spm is already partially in CSC, and we need it to compute the
+         * value indices
+         */
+        spm->fmttype = SpmCSC;
+        if ( spm->dof > 0 ) {
+            z_spm_dijv2csc_cdof( spm, oldrow, oldval, l2g_sorted );
+        }
+        else {
+            z_spm_dijv2csc_vdof( spm, oldrow, oldval, l2g_sorted );
+        }
+
+        free( oldrow );
+        if ( oldval ) {
+            free( oldval );
+        }
+    }
+    spm->fmttype = SpmCSC;
+    free(l2g_sorted);
+
+    return SPM_SUCCESS;
+}
+#endif
+
 /**
  *******************************************************************************
  *
@@ -44,15 +328,6 @@ z_spmConvertIJV2CSC( spmatrix_t *spm )
 {
     spm_int_t *newcol, *oldcol;
     spm_int_t  k, j, tmp, baseval, total;
-    spmatrix_t oldspm;
-
-    /* Backup the input */
-    memcpy( &oldspm, spm, sizeof(spmatrix_t) );
-
-    /*
-     * Check the baseval, we consider that arrays are sorted by columns or rows
-     */
-    baseval = spm->baseval;
 
     /*
      * Sort the IJV structure by column/row indexes
@@ -61,51 +336,26 @@ z_spmConvertIJV2CSC( spmatrix_t *spm )
 
 #if defined(SPM_WITH_MPI)
     if ( spm->loc2glob != NULL ) {
-        const spm_int_t *glob2loc;
-        spm_int_t        jg;
-        int              distribution = spm_get_distribution( spm );
-
-        if ( !(distribution & SpmDistByColumn) ) {
-            fprintf( stderr, "spmConvert: Conversion of non column distributed matrices to CSC is not yet implemented\n");
-            return SPM_ERR_BADPARAMETER;
-        }
-
-        /* Allocate and compute the glob2loc array */
-        glob2loc = spm_get_glob2loc( spm );
-
-        /* Allocate and compute the new colptr */
-        spm->colptr = (spm_int_t *) calloc(spm->n+1,sizeof(spm_int_t));
-
-        /* Compute the number of edges per row */
-        newcol = spm->colptr;
-        oldcol = oldspm.colptr;
-        for (k=0; k<spm->nnz; k++, oldcol++)
-        {
-            jg = *oldcol - baseval;
-            j  = glob2loc[ jg ];
-            assert( j >= 0 );
-            newcol[j]++;
-        }
+        return z_spm_dijv2csc( spm );
     }
-    else
 #endif
-    {
-        /* Allocate and compute the new colptr */
-        spm->colptr = (spm_int_t *) calloc(spm->n+1,sizeof(spm_int_t));
+    /* Allocate and compute the new colptr */
+    newcol = (spm_int_t *) calloc(spm->n+1,sizeof(spm_int_t));
 
-        /* Compute the number of edges per row */
-        newcol = spm->colptr;
-        oldcol = oldspm.colptr;
-        for (k=0; k<spm->nnz; k++, oldcol++)
-        {
-            j = *oldcol - baseval;
-            assert( j >= 0 );
-            newcol[j]++;
-        }
+    /* Compute the number of edges per row */
+    baseval = spm->baseval;
+    oldcol  = spm->colptr;
+    for (k=0; k<spm->nnz; k++, oldcol++)
+    {
+        j = *oldcol - baseval;
+        assert( j >= 0 );
+        newcol[j]++;
     }
+    free( spm->colptr );
 
     /* Update the colptr */
-    total = baseval;
+    total       = baseval;
+    spm->colptr = newcol;
     for (j=0; j<(spm->n+1); j++, newcol++)
     {
         tmp = *newcol;
@@ -113,8 +363,6 @@ z_spmConvertIJV2CSC( spmatrix_t *spm )
         total += tmp;
     }
     assert( (total - baseval) == spm->nnz );
-
-    free( oldspm.colptr );
     spm->fmttype = SpmCSC;
 
     return SPM_SUCCESS;
@@ -363,7 +611,7 @@ z_spmConvertCSR2CSC_gen( spmatrix_t *spm )
     if ( spm->dof != 1 ) {
         spm_int_t *colcsr;
         spm_int_t *rowtmp = row_csc;
-        spm_int_t *validx = spm_create_asc_values( spm );
+        spm_int_t *validx = spm_get_value_idx_by_elt( spm );
         spm_int_t  dof, idx;
         spm_int_t  dofi, dofj, dof2;
 
