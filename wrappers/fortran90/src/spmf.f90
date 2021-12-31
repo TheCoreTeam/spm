66439ecab45d9bb7bab28a021a3dc1b35d89e4ed
Mathieu Faverge <mathieu.faverge@inria.fr>
2021-12-31 13:18:17 +0100

    spmf.f90: Makes it a comme module file to include all other submodules

---
+++ wrappers/fortran90/src/spmf.f90
@@ -0,0 +1,23 @@
+!>
+!> @file spmf.f90
+!>
+!> SPM Fortran 90 module
+!>
+!> @copyright 2017-2021 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+!>                      Univ. Bordeaux. All rights reserved.
+!>
+!> @version 1.1.0
+!> @author Mathieu Faverge
+!> @author Tony Delarue
+!> @date 2021-12-31
+!>
+!> This file has been automatically generated with gen_wrappers.py
+!>
+!> @ingroup wrap_fortran
+!>
+module spmf
+
+  use spmf_enums
+  use spmf_interfaces
+
+end module spmf
