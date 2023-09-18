b4dae138f0cceedeab79760db3546512761ff09b
Florent Pruvost <florent.pruvost@inria.fr>
2023-09-18 15:53:10 +0200

    Add windows jobs for build and test. Reorganize ci jobs for readibility.

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -7,14 +7,18 @@ fatal() {
 
 set -x
 
-mkdir -p build
-cd build
-echo $VERSION | tee ../spm-build-${VERSION}.log
-if [[ "$CC" == "clang" ]]; then
-  # clang is used on macosx and it is not compatible with MORSE_ENABLE_COVERAGE=ON
-  cmake -DSPM_CI_VERSION=${VERSION} -DSPM_CI_BRANCH=${BRANCH} -C ../.gitlab/ci-test-initial-cache.cmake -DMORSE_ENABLE_COVERAGE=OFF ..  || fatal
+if [[ "$SYSTEM" != "windows" ]]; then
+  if [[ "$SYSTEM" == "macosx" ]]; then
+    # clang is used on macosx and it is not compatible with MORSE_ENABLE_COVERAGE=ON
+    cmake -B build -S . -DSPM_CI_VERSION=${VERSION} -DSPM_CI_BRANCH=${BRANCH} -C .gitlab/ci-test-initial-cache.cmake -DMORSE_ENABLE_COVERAGE=OFF || fatal
+  else
+    cmake -B build -S . -DSPM_CI_VERSION=${VERSION} -DSPM_CI_BRANCH=${BRANCH} -C .gitlab/ci-test-initial-cache.cmake || fatal
+  fi
+  cmake --build build -j 4 || fatal
+  cmake --install build || fatal
 else
-  cmake -DSPM_CI_VERSION=${VERSION} -DSPM_CI_BRANCH=${BRANCH} -C ../.gitlab/ci-test-initial-cache.cmake ..  || fatal
+  # on windows the mpi_f08 interface is missing, see https://www.scivision.dev/windows-mpi-msys2/
+  cmake -GNinja -B build -S . -DCMAKE_INSTALL_PREFIX=$PWD/install-${VERSION} -DBUILD_SHARED_LIBS=ON -DSPM_WITH_MPI=OFF || fatal
+  cmake --build build -j 4 || fatal
+  cmake --install build || fatal
 fi
-make -j 4    | tee -a ../spm-build-${VERSION}.log  || fatal
-make install | tee -a ../spm-build-${VERSION}.log  || fatal
