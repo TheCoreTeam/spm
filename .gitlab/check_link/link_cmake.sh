451ca1f86a4bf307650d0b9ea2d93cb6af4b4cfd
Mathieu Faverge <mathieu.faverge@inria.fr>
2023-11-29 23:04:18 +0100

    ci: Add headers to CI scripts and cleanup coding style

--- .gitlab/check_link/link_cmake.sh
+++ .gitlab/check_link/link_cmake.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
-# usage: ./link_cmake.sh path_to_spm_install
+#!/usr/bin/env sh
+###
+#
+#  @file link_cmake.sh
+#  @copyright 2023-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+#                       Univ. Bordeaux. All rights reserved.
+#
+#  @version 1.2.1
+#  @author Florent Pruvost
+#  @author Mathieu Faverge
+#  @date 2023-11-29
+#
+# Check that linking with the project is ok when using cmake.
+#
+###
 set -ex
+
+if [ $# -lt 1 ]
+then
+    echo "usage: ./link_cmake.sh path_to_spm_install"
+    exit 1
+fi
+
 cmake -B build -DCMAKE_PREFIX_PATH=$1
-cmake --build build --verbose
+cmake --build    build --verbose
 ctest --test-dir build --verbose
--- .gitlab/check_link/link_pkgconfig.sh
+++ .gitlab/check_link/link_pkgconfig.sh
@@ -1,26 +1,63 @@
-#!/bin/sh
-# usage: ./link_pkgconfig.sh path_to_spm_install [--static]
-# use the --static parameter if spm is static (.a)
-# env. var. CC and FC must be defined to C and Fortran90 compilers
+#!/usr/bin/env sh
+###
+#
+#  @file link_pkgconfig.sh
+#  @copyright 2023-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+#                       Univ. Bordeaux. All rights reserved.
+#
+#  @version 1.2.1
+#  @author Florent Pruvost
+#  @author Mathieu Faverge
+#  @date 2023-11-29
+#
+# Check that linking with the project is ok when using pkg-config.
+#
+###
 set -ex
 
-export PKG_CONFIG_PATH=$1/lib/pkgconfig:$PKG_CONFIG_PATH
+static=""
+spm_path=""
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        --static | -static | -s )
+            static="--static"
+            ;;
+        * )
+            spm_path=$1
+            ;;
+    esac
+    shift
+done
+
+if [ -z $spm_path ]
+then
+    echo """
+usage: ./link_pkgconfig.sh path_to_spm_install [--static|-static|-s]
+   use the --static parameter if spm is static (.a)
+   env. var. CC and FC must be defined to C and Fortran90 compilers
+"""
+    exit 1
+fi
+
+export PKG_CONFIG_PATH=$spm_path/lib/pkgconfig:$PKG_CONFIG_PATH
 
 mkdir -p build
 cd build
 
-FLAGS=`pkg-config $2 --cflags spm`
+FLAGS=`pkg-config $static --cflags spm`
 if [[ "$SYSTEM" == "macosx" ]]; then
-  FLAGS="-Wl,-rpath,$1/lib $FLAGS"
+  FLAGS="-Wl,-rpath,$spm_path/lib $FLAGS"
 fi
-LIBS=`pkg-config $2 --libs spm`
+LIBS=`pkg-config $static --libs spm`
 $CC $FLAGS ../../../examples/example_drivers.c $LIBS -o link_spm_c
 ./link_spm_c -t 2 --lap 100
 
-FLAGS=`pkg-config $2 --cflags spmf`
+FLAGS=`pkg-config $static --cflags spmf`
 if [[ "$SYSTEM" == "macosx" ]]; then
-  FLAGS="-Wl,-rpath,$1/lib $FLAGS"
+  FLAGS="-Wl,-rpath,$spm_path/lib $FLAGS"
 fi
-LIBS=`pkg-config $2 --libs spmf`
+LIBS=`pkg-config $static --libs spmf`
 $FC $FLAGS ../../../wrappers/fortran90/examples/spmf_driver.F90 $LIBS -o link_spm_f
 ./link_spm_f --lap 100
